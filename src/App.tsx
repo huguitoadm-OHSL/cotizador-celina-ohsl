173a15bafa8f7641978aa540a61b9e7b5ca86d0e
huguitoadm-OHSL <huguitoadm@gmail.com>
2026-08-06 06:42:40 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -235,7 +235,7 @@ export default function App() {
   
   const handleLogin = (e) => {
     e.preventDefault();
-    if (passwordInput === "ELSEÑORESMIPASTOR") {
+    if (passwordInput === "DIOSESMIGUIA") {
       setIsAuthenticated(true); 
       setIsAdmin(false); 
       setLoginError(false);
@@ -258,7 +258,7 @@ export default function App() {
   const [usarBD, setUsarBD] = useState(true);
 
   const [tipoCotizacion, setTipoCotizacion] = useState("credito"); 
-  const [tcFlexible, setTcFlexible] = useState(12.08);
+  const [tcFlexible, setTcFlexible] = useState(11.86);
   const TC_PROMOCIONAL = 6.97;
 
   const [uv, setUv] = useState("");
@@ -499,7 +499,7 @@ export default function App() {
     let valor_final = 0, ahorro_total = 0, cuota_inicial = 0, pct_efectivo = 0, pago_puro = 0, seguro = 0, cbdi = 0, cuota_final = 0;
     let planPagosArreglo = [], transicionData = [], totalAhorroTransicion = 0;
     let ahorro_contra_mercado = 0, costo_esperar_octubre = 0, descPctOct = 0;
-    const TC_FLEX_NUMBER = Number(tcFlexible) || 12.08;
+    const TC_FLEX_NUMBER = Number(tcFlexible) || 11.86;
 
     if (tipoCotizacion === 'contado') {
         const descContadoM2Val = aplicarDescContadoM2 ? (Number(descuentoContadoM2) || 0) : 0;
