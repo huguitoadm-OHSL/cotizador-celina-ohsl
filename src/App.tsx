6175dfd6cd878bdb41f3092be894792f35cfff2d
Oscar H. Saravia <huguitoadm@gmail.com>
2026-09-09 09:41:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -101,6 +101,8 @@ const baseAnclasUrbanas = {
     { id: 'equipamiento-M47', nombre: 'Equipamiento Público', tipo: 'comercio', lat: -17.36589498764944, lng: -63.29634648611603 },
     { id: 'equipamiento-M56', nombre: 'Equipamiento Público', tipo: 'comercio', lat: -17.366662848620702, lng: -63.29006107676353 },
     { id: 'equipamiento-M64', nombre: 'Equipamiento Público', tipo: 'comercio', lat: -17.369702946712444, lng: -63.29470631145279 },
+    { id: 'mercado-germán-moreno', nombre: 'Mercado Germán Moreno', tipo: 'comercio', lat: -17.343231912647827, lng: -63.248839462598916 },
+    { id: 'plaza-principal-montero', nombre: 'Plaza Principal de Montero', tipo: 'recreacion', lat: -17.341993229014623, lng: -63.25572464742824 },
   ],
   "URUBÓ NORTE": [
     { id: 'puente-foianini', nombre: 'Puente Mario Foianini', tipo: 'landmark', lat: -17.7551, lng: -63.2045 },
@@ -375,7 +377,7 @@ export default function App() {
   const [cargandoBD, setCargandoBD] = useState(true);
   const [usarBD, setUsarBD] = useState(true);
   const [tipoCotizacion, setTipoCotizacion] = useState("credito"); 
-  const [tcFlexible, setTcFlexible] = useState(12.60); 
+  const [tcFlexible, setTcFlexible] = useState(12.64); 
   
   const [uv, setUv] = useState("");
   const [mzn, setMzn] = useState("");
